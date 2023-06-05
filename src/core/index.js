f4705f0056c4122918f999ff6cb82f044098c1b7
Elena Miller <evmpr0gramm1ng@outlook.com>
2023-06-05 17:46:19 -0500

    edited module files
    Signed-off-by: Elena Miller <evmpr0gramm1ng@outlook.com>

---
+++ src/core/index.js
@@ -0,0 +1,12 @@
+/**
+    * @description      : 
+    * @author           : eVmPr
+    * @group            : 
+    * @created          : 05/06/2023 - 16:10:36
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 05/06/2023
+    * - Author          : eVmPr
+    * - Modification    : 
+**/
